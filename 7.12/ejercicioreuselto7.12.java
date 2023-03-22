0b18e16e7279b3fd48f60e6656d1dbfe2c25a91d
evaipp <126181883+evaipp@users.noreply.github.com>
2023-03-22 20:04:13 +0100

    Add files via upload

---
+++ 7.12/ejercicioreuselto7.12.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lista l1; new Lista();
+        Lista l2; new Lista();
+        l1. insertarFinal(nuevol 1);
+        l2. insertarFinal(nuevol 3);
+        l1. insertarFinal(nuevol 10);
+        Lista concatenacion = Lista.concatenar(l1, l2);
+        concatenacion. mostrar();
+
+       
+    }
+}
---
+++ 7.9/Bombilla7..java
@@ -0,0 +1,31 @@
+
+public class Bombilla {
+  public static boolean interruptorGeneral = true; 
+  private boolean interruptor;
+  public Bombilla() {
+    interruptor = false;
+
+}
+
+  public void enciende() {
+    interruptor = true;
+
+  }
+   public void apagar() {
+    interruptor = false;
+  }
+
+  public boolean estado() {
+    return interruptorGeneral && interruptor;
+
+  }
+  public String muestraEstado() {
+    return estado() ? "Encendida" : "Apagada";
+
+
+  }
+
+public void apaga() {
+}
+
+}
