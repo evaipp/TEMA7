0099968690af75b641a03224d8faeddc978559d2
evaipp <126181883+evaipp@users.noreply.github.com>
2023-04-24 13:09:57 +0200

    Add files via upload

---
+++ 7.10/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    import org.w3c.dom.Text;
+
+    Texto t = new Texto(5);t.addPrincipio("HO");t.addPrincipio(';');t.addFinal("Lá");t.addFinal('X');t.mostrar();System.out.println("Número de vocales: "+t.numVocales());
+}}}}
